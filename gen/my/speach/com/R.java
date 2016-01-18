11800fb31967a667b24293872659acaf51e5f4ff
root <root@localhost.localdomain>
2016-01-18 17:39:46 +0300

    fix small bugs

--- gen/my/speach/com/R.java
+++ gen/my/speach/com/R.java
@@ -56,10 +56,12 @@ public final class R {
         public static final int action_settings=0x7f050001;
         public static final int action_settings2=0x7f050002;
         public static final int app_name=0x7f050000;
-        public static final int info=0x7f050004;
-        public static final int info2=0x7f050005;
-        public static final int info3=0x7f050006;
-        public static final int info4=0x7f050007;
-        public static final int title_activity_settings=0x7f050003;
+        public static final int applist=0x7f050004;
+        public static final int info=0x7f050006;
+        public static final int info2=0x7f050007;
+        public static final int info3=0x7f050008;
+        public static final int info4=0x7f050009;
+        public static final int title_activity_settings=0x7f050005;
+        public static final int whiteblack=0x7f050003;
     }
 }
